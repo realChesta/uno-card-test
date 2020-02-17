1e2d9f26e572409e821772174e16c353d3f64265
Chesta <chesta@protonmail.com>
2020-02-17 17:50:22 +0100

    added card component

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/Card.css'
+
+class Card extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="card-container">
+
+            </div>
+        );
+    }
+}
+
+export default Card;
