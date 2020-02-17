05f163334b3b98bc866e4782080403bad353017b
Chester <chesta@kekt.us>
2020-02-17 23:42:59 +0100

    Number cards mostly work and look fine now

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Card from './components/Card';
+
+// standard playing card size:
+// 62mm x 82mm
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Uno Card Prototype
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card number="7" color="green"/>
       </header>
-      <body>
-        <Card/>
-      </body>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/Card.css'
+import '../styles/Colors.css'
 
 class Card extends React.Component {
     constructor() {
@@ -7,9 +8,19 @@ class Card extends React.Component {
     }
 
     render() {
-        return (
-            <div className="card-container">
+        let center;
+        if (isNaN(this.props.number)) {
 
+        }
+        else {
+            center = <div className="card-center number">{this.props.number}</div>;
+        }
+        return (
+            <div className={"card-container " + this.props.color}>
+                <div className="card-glare"/>
+                <div className="card-border-icon top">{this.props.number}</div>
+                {center}
+                <div className="card-border-icon bottom">{this.props.number}</div>
             </div>
         );
     }
